606889cb0420bbe9e8459f9cd86afa972f070871
ivan88217 <ivan88217@gmail.com>
2024-05-13 22:55:59 +0800

    prisma crud example

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './logger/logger.module';
 import { LoggingModule } from './logging/logging.module';
+import { PrismaService } from './prisma/prisma.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [LoggerModule, LoggingModule],
+  imports: [LoggerModule, LoggingModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
