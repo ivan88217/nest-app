a74f46881e9612c9877fda840e2bc68080093250
ivan88217 <ivan88217@gmail.com>
2024-05-13 23:37:13 +0800

    add exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerService } from '@/logger/logger.service';
+import { AllExceptionsFilter } from '@/all-exceptions/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   const logger = app.get(LoggerService);
   app.useLogger(logger);
+  app.useGlobalFilters(new AllExceptionsFilter());
   const port = process.env.PORT || 3000;
   await app.listen(3000, () =>
     logger.log(`Server is running on http://localhost:${port}}`),
